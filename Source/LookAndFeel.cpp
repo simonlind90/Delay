bb713e0e7830e3862d67dd76dd2a0c56e33010a0
Simon Lind <simonlind90@gmail.com>
2024-10-16 22:34:59 +0200

    Drawing the knob

--- Source/LookAndFeel.cpp
+++ Source/LookAndFeel.cpp
@@ -15,7 +15,21 @@ RotaryKnobLookAndFeel::RotaryKnobLookAndFeel() {
     setColour(juce::Slider::textBoxTextColourId, Colors::Knob::label);
 }
 
-void RotaryKnobLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height,
+void RotaryKnobLookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, [[maybe_unused]] int height,
                                              float sliderPos, float rotaryStartAngle, float rotaryEndAngle, juce::Slider &slider) {
+    auto bounds = juce::Rectangle<int>(x, y, width, width).toFloat();
+    auto knobRect = bounds.reduced(10.0f, 10.0f);
     
+    auto path = juce::Path();
+    path.addEllipse(knobRect);
+    dropShadow.drawForPath(g, path);
+    
+    g.setColour(Colors::Knob::outline);
+    g.fillEllipse(knobRect);
+    
+    auto innerRect = knobRect.reduced(2.0f, 2.0f);
+    auto gradient = juce::ColourGradient(Colors::Knob::gradientTop, 0.0f, innerRect.getY(),
+                                         Colors::Knob::gradientBottom, 0.0f, innerRect.getBottom(), false);
+    g.setGradientFill(gradient);
+    g.fillEllipse(innerRect);
 }
--- Source/LookAndFeel.h
+++ Source/LookAndFeel.h
@@ -45,5 +45,6 @@ public:
                           float sliderPos, float rotaryStartAngle, float rotaryEndAngle,
                           juce::Slider& slider) override;
 private:
+    juce::DropShadow dropShadow { Colors::Knob::dropShadow, 6, { 0, 3 } };
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(RotaryKnobLookAndFeel)
 };
