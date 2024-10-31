aeae13ae8eb17177394ca6ab908c8ffee6dfeb76
Simon Lind <simonlind90@gmail.com>
2024-10-31 13:02:08 +0100

    Drawing images

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -43,14 +43,26 @@ DelayAudioProcessorEditor::~DelayAudioProcessorEditor() {
 //==============================================================================
 void DelayAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    g.fillAll(Colors::background);
+    auto noise = juce::ImageCache::getFromMemory(BinaryData::Noise_png, BinaryData::Noise_pngSize);
+    auto fillType = juce::FillType(noise, juce::AffineTransform::scale(0.5f));
+    g.setFillType(fillType);
+    g.fillRect(getLocalBounds());
+    auto rect = getLocalBounds().withHeight(40);
+    g.setColour(Colors::header);
+    g.fillRect(rect);
+    
+    auto image = juce::ImageCache::getFromMemory(BinaryData::Logo_png, BinaryData::Logo_pngSize);
+    int destWidth = image.getWidth() / 2;
+    int destHeight = image.getHeight() / 2;
+    g.drawImage(image, getWidth() / 2 - destWidth / 2, 0, destWidth, destHeight,
+                0,0, image.getWidth(), image.getHeight());
 }
 
 void DelayAudioProcessorEditor::resized()
 {
     auto bounds = getLocalBounds();
-    int y = 10;
-    int height = bounds.getHeight() - 20;
+    int y = 50;
+    int height = bounds.getHeight() - 60;
     
     // Position the groups
     delayGroup.setBounds(10, y, 110, height);
